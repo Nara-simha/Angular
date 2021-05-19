f8a73e2565b505ab0fed7e57ccc374f02e9aee79
Narasimha <narasimhabodepudi@gmail.com>
2021-05-19 19:20:47 +0530

    routing

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent{
+  @Input() productObj:Product;
 
-  constructor() { }
+  // custom event
+  @Output() myEvent= new EventEmitter();
 
-  ngOnInit(): void {
+  addProduct(productName){
+    this.myEvent.emit(productName);
   }
-
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProductsComponent {
+  products:Product[]=[
+    {
+      productTitle:"Iphone 12 Pro Max",
+      description:"1,38,999.00",
+      productImage:"https://images-na.ssl-images-amazon.com/images/I/71MHTD3uL4L._SL1500_.jpg"
+    },
+    {
+      productTitle:"Iphone 12 Pro",
+      description:"1,19,900.00",
+      productImage:"https://m.media-amazon.com/images/I/71cSV-RTBSL._SL1500_.jpg"
+    },
+    {
+      productTitle:"Iphone 12",
+      description:"77,900.00",
+      productImage:"https://m.media-amazon.com/images/I/71E5zB1qbIL._SL1500_.jpg"
+    },
+    {
+      productTitle:"Oneplus 9 Pro",
+      description:"64.999.00",
+      productImage:"https://m.media-amazon.com/images/I/612ytK4luvL._SL1500_.jpg"
+    },
+    {
+      productTitle:"Oneplus 9",
+      description:"54,999.00",
+      productImage:"https://m.media-amazon.com/images/I/61fy+u9uqPL._SL1500_.jpg"
+    },
+    {
+      productTitle:"Oneplus 9R",
+      description:"39,999.00",
+      productImage:"https://images-na.ssl-images-amazon.com/images/I/61IwksZ-DGL._SL1500_.jpg"
+    }
+  ];
+  productSelected=[];
+  c=0;
+  getProductDetails(productName){
+    this.productSelected.push(productName)
+    this.c++;
   }
-
 }
